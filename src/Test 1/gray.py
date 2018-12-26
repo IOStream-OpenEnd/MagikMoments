7fce2fdcf80786cbd31d66d9c0f25c61250e4fbe
Yahyaa Abdullah <kogam22@gmail.com>
2018-12-26 23:46:41 +0530

    implement create frame and combine

--- src/Test 1/gray.py
+++ src/Test 1/gray.py
@@ -1,5 +1,19 @@
 import numpy as np
 import cv2
+import create
+import combine
+import time
+
+video_file = 'abc.mp4'
+for img in create_frames("abc.mp4"):
+    # This will display the each frame 100 secs
+    #cv2.imshow('image',img)
+    #cv2.waitKey(0)
+    #time.sleep(10)
+
+    # img can be used directly for further processing
+    pass
+
 
 #importing the All the face-detecting Cascade
 face_detector1 = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -8,9 +22,11 @@ face_detector3 = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 face_detector4 = cv2.CascadeClassifier("haarcascade_frontalface_alt_tree.xml")
 
 #Reading the file
-img = cv2.imread('index.jpeg')
+img = cv2.imread("index.jpeg") #This line is not required because of the create_frames sends us images
+
 #Converting it to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 #Detect face using 4 different classifiers
 faces1 = face_detector1.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
 faces2 = face_detector2.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
@@ -60,3 +76,13 @@ try:
     cv2.imwrite("index4.jpeg", out) #Write image
 except:
     pass
+
+
+
+frames_directory = 'frames/'
+save_name = 'abc.mp4'
+fps = 25.0
+convert_frames_to_video(frames_directory, save_name, fps)
+
+# Finally
+#cv.destroyAllWindows()
