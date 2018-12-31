c5eee154021c7b9c2c15a8565b55269179ea423e
Hidayat Ullah Khan <huk2209@gmail.com>
2018-12-31 21:17:20 +0530

    test_combine_clips function added

--- src/Test1/test.py
+++ src/Test1/test.py
@@ -3,9 +3,10 @@ import os
 import cv2
 import classifier,trim_merge
 from moviepy.editor import VideoFileClip
+from nose.tools import assert_almost_equals
 
 class Tests(unittest.TestCase):
-
+	"""
 	def test_create_frames(self):
 		# fetch video
 		vid=cv2.VideoCapture("sample_video.mp4")
@@ -27,7 +28,7 @@ class Tests(unittest.TestCase):
 		timestamps=[x for x in range(0,int(clip.duration),5)]
 
 		# calling cut_moments
-		tm.cut_moments(timestamps,"sample_video.mp4")
+		tm.cut_moments("sample_video.mp4",timestamps)
 
 		# if clips folder exists, the number of timestamps should be equal to number of subclips generated
 		if os.path.exists("clips"):
@@ -36,6 +37,37 @@ class Tests(unittest.TestCase):
 		else:
 			print("clips folder does not exist")
 			self.assertEqual(1,-1)
+	"""
+	def test_combine_clips(self):
+    	# CombineClips object
+		tm=trim_merge.CombineClips()
+
+		# fetching the video clip
+		clip=VideoFileClip("sample_video.mp4")
+
+		# building timestamps list with each timestamp atleast 5 seconds away from the other one
+		timestamps=[x for x in range(0,int(clip.duration),5)]
+
+		# calling cut_moments
+		tm.cut_moments("sample_video.mp4",timestamps)
+
+		#calling combine_clips
+		tm.combine_clips("some_file.mp4")
+
+		if not os.path.isfile("some_file.mp4"):
+			self.assertEqual(1,-1)
+		else:
+			video_names=[("clips/" + n) for n in os.listdir('clips/') if n[:4]=="clip" and n[-4:]=='.mp4']
+			intro_clip = VideoFileClip(f"intro_1.mp4")
+			end_clip = VideoFileClip(f"the_end_1.mp4")
+			dur=0
+			for clip_name in video_names:
+				clip = VideoFileClip(clip_name)
+				dur+=clip.duration
+			dur+=intro_clip.duration
+			dur+=end_clip.duration
+			outputvideo = VideoFileClip("some_file.mp4")
+			assert_almost_equals(dur, outputvideo.duration, places=1)
     	
     	
     # def test_detect_face(self):
